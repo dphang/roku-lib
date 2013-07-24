2547f4fcdb00b1d1c2dc2fcc3feb39d29356bf28
dphang <dphang12@gmail.com>
2013-07-24 03:07:18 -0700

    Added (incomplete) Python roku minifier.

---
+++ Python/minifier.py
@@ -0,0 +1,23 @@
+# Python script to minify a Roku project
+# Pass in the source directory, and all files will be minified and optimized
+# All functions belonging to an object will be made anonymous, and whitespace will be deleted where possible
+# Comments will be deleted
+# Function names will be shortened
+
+class RokuMinifier:
+    
+    def __init__(self, directory):
+   
+    def findFunctions(self, file):
+        '''
+        Find all functions
+        '''
+
+    def deleteComments(self, file):
+        for line in self.lines:
+            
+        
+        
+        
+        
+    
